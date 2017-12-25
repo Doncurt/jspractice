001be67a15c4716a34d0b6ff5cc773d8e48452bf
Donovan Adams <donovan.adams@students.makeschool.com>
2017-12-24 20:01:36 -0800

    starting up heavy js pratice again

---
+++ practscript.js
@@ -0,0 +1,12 @@
+let myArray = ['1','2','3','4','5'];
+console.log(myArray);
+
+console.log(myArray.length);
+
+console.log(myArray[0] + myArray[1])
+
+myArray.push('6','7');
+
+console.log(myArray);
+
+myArray.
