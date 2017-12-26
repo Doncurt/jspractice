6dfb4d9410f00d3242826fd985acb09a3ffa5a1b
Donovan Adams <donovan.adams@students.makeschool.com>
2017-12-25 19:56:31 -0800

    commiting everythng up to objects and classes

---
+++ classPract.js
@@ -0,0 +1,17 @@
+class Animal{
+  constructor(name){
+    this.name= name
+    }
+    get behavior(){
+      return this.behavior;
+    }
+
+}
+
+class Cat extends Animal{
+  constructor(name, usesLitterBox)
+    super(name);
+    this.usesLitterBox= usesLitterBox
+  }
+get
+}
--- loopsPract.js
+++ loopsPract.js
@@ -3,3 +3,25 @@ const myArray = ['1','2','3','4','5'];
 for(let i=0; i < myArray.length; i++){
   console.log(myArray[i]);
 }
+/*printing backwards*/
+
+for(let i = myArray.length-1;i >=0;i--){
+  console.log(myArray[i]);
+}
+
+/*NESTED CHDCKS FOR EQUALITY*/
+
+let myPlaces =['nyc','miami','sf'];
+let friendPlaces= ['nyc','bali','Yo mamas house'];
+
+for(let myPlacesIndex =0; myPlacesIndex < myPlaces.length; myPlacesIndex++){
+
+  for(let friendPlacesIndex =0; friendPlacesIndex < friendPlaces.length; friendPlacesIndex++)		{
+
+  		if(myPlaces[myPlacesIndex] === friendPlaces[friendPlacesIndex]){
+        console.log(myPlaces[myPlacesIndex]);
+      }
+
+	}
+
+}
