aee89bfb55399694eaa1b7ec358173cbc65c5105
Donovan Adams <donovan.adams@students.makeschool.com>
2017-12-30 15:52:47 -0800

    commiting the class and variable practice for es5 before switchng to es6

--- classPract.js
+++ classPract.js
@@ -9,8 +9,24 @@ class Animal{
 }
 
 class Cat extends Animal {
-  constructor(name, usesLitterBox){}
+  constructor(name, usesLitterBox){
     super(name);
     this._usesLitterBox= usesLitterBox
   }
 }
+
+
+
+var person = {
+  firstName : "Donovan",
+  lastName : " adams",
+  fullName: function (){
+    return this.firstName + " " + this. lastName
+  }
+
+}
+var mary ={
+  firstName: "mary",
+  lastName: "smith",
+}
+console.log(person.fullName.call(mary));
